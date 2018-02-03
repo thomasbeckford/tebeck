f56001f08a7be9bfc4790ef94b9c15c214c390d9
tebeck <tebeckford@gmail.com>
2018-02-03 20:21:11 -0300

    asdf

--- hornobar/index.php
+++ hornobar/index.php
@@ -1,18 +1,19 @@
 <!DOCTYPE HTML>
-<html>
+<html xmlns:fb="http://ogp.me/ns/fb#">
+
 <head>
 	<title>Hornobar</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="pragma" content="no-cache" />
 	<meta http-equiv="expires" content="-1" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
-
+	
 </head>
 
 <body>
 
  
-<fb:like href="http://www.geeks.gallery/" layout="standard" action="like" show_faces="true" share="false"></fb:like>
+<fb:like href="https://www.facebook.com/Hornobar-939390592790568/" layout="standard" action="like" show_faces="true" share="false"></fb:like>
 <div id="fb-root"></div>
 
 
@@ -22,7 +23,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=245126446025547&version=v2.1";
+  js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&appId=245126446025547&version=v2.1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 </script>
@@ -36,14 +37,15 @@ window.fbAsyncInit = function() {
   FB.Event.subscribe('edge.create', function(response) {
     // like clicked
     $.ajax({
-      url: "path of a php file in server to insert user data in your database",
+      url: "https://tebeck.com.ar/asdf.php",
       type: "post",
       data: {type:'like',uid:'login session user id'},
       success: function(data){
        // On successful Response.
+       console.log("redirigiendo..");
       },
       error:function(){
-      // On Error.
+      console.log("error")..;
        }   
     }); 
   });
