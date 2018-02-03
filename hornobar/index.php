1416271a8396e896043811ce793ef54672cf4d99
tebeck <tebeckford@gmail.com>
2018-02-03 20:15:58 -0300

    asdf

--- hornobar/index.php
+++ hornobar/index.php
@@ -12,63 +12,45 @@
 
 <body>
 
- <div id="fb-root"></div>
-<script src="https://connect.facebook.net/en_US/all.js"> </script>
-
-<script>
-
-	FB.init({
-	appId :'245126446025547',
-			status : true,
-			cookie : true,
-			xfbml : true,
-	});
-
-	FB.getLoginStatus(function(response) {
-		if (response.status == "connected"){
-			console.log("connected");
-		FB.api('/me/likes/939390592790568', function(response) {
-		    console.log(response.data);
-		    if(response.data == ''){
-		    }else{
-		    	alert("Redirigiendo mono funciona...")
-		    	document.getElementById('redi').innerHTML = "REDIRIGIENDO....";
-
-				//redirect();
-
-		    }
-		});
-
-	}
-});
+ 
+<fb:like href="http://www.geeks.gallery/" layout="standard" action="like" show_faces="true" share="false"></fb:like>
+<div id="fb-root"></div>
 
 
+ <script>
 
-(function(d, s, id) {
+ 	(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v2.5&appId=245126446025547";
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=245126446025547&version=v2.1";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-
-function redirect(){
-
-	top.window.location = "https://www.google.com.ar/search?ei=6MFsWpDKJ8L-wQS306TwBQ&q=hornobar&oq=hornobar&gs_l=psy-ab.3..0i67k1j0i10k1j0i67k1j0j0i10k1j0i10i203k1l2j0j0i30k1l2.1052.1761.0.1837.8.8.0.0.0.0.152.704.4j3.7.0....0...1c.1.64.psy-ab..1.7.702...35i39k1j0i131k1j0i131i67k1j0i203k1.0.07HhwIqi1EM#lrd=0x95bcb1c96860a617:0x78666d8ff55c8a1f,1";
-	}
-
+</script>
+<script>
+window.fbAsyncInit = function() {
+  FB.init({
+    appId      : '245126446025547',
+    xfbml      : true,
+    version    : 'v2.1'
+  });
+  FB.Event.subscribe('edge.create', function(response) {
+    // like clicked
+    $.ajax({
+      url: "path of a php file in server to insert user data in your database",
+      type: "post",
+      data: {type:'like',uid:'login session user id'},
+      success: function(data){
+       // On successful Response.
+      },
+      error:function(){
+      // On Error.
+       }   
+    }); 
+  });
+};    
 </script>
 
-
-<input type="hidden" name="actionlike" id="actionlike" value="<?php echo $actionlike; ?>" />
-<input type="hidden" name="dst" id="dst" value="<?php echo $dst; ?>" />
-
-<h3 class="box-like-h">Accede a WIFI dandonos un like en facebook!</h3><br>
-
-<p id="redi"></p>
-
-<div id="btn" class="fb-like" data-href="https://www.facebook.com/Hornobar-939390592790568" data-layout="standard" data-action="like" data-show-faces="false" data-share="false"></div>
 
 <footer>
 
