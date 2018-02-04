169fb6c72e451e4f75cb6bda436fed465c7eb0e6
tebeck <tebeckford@gmail.com>
2018-02-04 13:09:34 -0300

    asd

--- hornobar/index.php
+++ hornobar/index.php
@@ -1,3 +1,23 @@
 <?php
 
 print_r($_GET);
+
+
+
+/* PHP SDK v5.0.0 */
+/* make the API call */
+try {
+  // Returns a `Facebook\FacebookResponse` object
+  $response = $fb->get(
+    '/{object-id}/likes',
+    '{code}'
+  );
+} catch(Facebook\Exceptions\FacebookResponseException $e) {
+  echo 'Graph returned an error: ' . $e->getMessage();
+  exit;
+} catch(Facebook\Exceptions\FacebookSDKException $e) {
+  echo 'Facebook SDK returned an error: ' . $e->getMessage();
+  exit;
+}
+$graphNode = $response->getGraphNode();
+/* handle the result */
